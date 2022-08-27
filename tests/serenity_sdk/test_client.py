6c4facd9e122025d137b81be21fb70055afb9ae6
Kyle F. Downey <kyle.downey@cloudwall.capital>
2022-08-27 18:17:52 -0400

    Removed Makefile and migrated to GitHub Actions
    SERENITY-1005

--- tests/serenity_sdk/test_client.py
+++ tests/serenity_sdk/test_client.py
@@ -6,4 +6,4 @@ from serenity_sdk.client import load_local_config
 def test_load_local_config():
     config_dir = os.path.dirname(__file__)
     config = load_local_config('test_config', config_dir=config_dir)
-    assert(config['schemaVersion'] == 1)
+    assert config['schemaVersion'] == 1
